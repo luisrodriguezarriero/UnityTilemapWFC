b970884b14765cc6b2b0f7663bf108f3f405ae2c
Luyso <luiskbx@gmail.com>
2022-11-21 19:04:22 +0100

    Initial commit

---
+++ Assets/Scripts/wfc/Helper.cs
@@ -0,0 +1,80 @@
+﻿// Copyright (C) 2016 Maxim Gumin, The MIT License (MIT)
+
+using System.Linq;
+using System.Xml.Linq;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System;
+using UnityEngine.Tilemaps;
+
+
+static class Helper
+{
+    public static int Random(this double[] weights, double r)
+    {
+        double sum = 0;
+        for (int i = 0; i < weights.Length; i++) sum += weights[i];
+        double threshold = r * sum;
+
+        double partialSum = 0;
+        for (int i = 0; i < weights.Length; i++)
+        {
+            partialSum += weights[i];
+            if (partialSum >= threshold) return i;
+        }
+        return 0;
+    }
+
+    public static long ToPower(this int a, int n)
+    {
+        long product = 1;
+        for (int i = 0; i < n; i++) product *= a;
+        return product;
+    }
+
+    public static T[] SubArray<T>(this T[] array, int offset, int length)
+    {
+        T[] result = new T[length];
+        Array.Copy(array, offset, result, 0, length);
+        return result;
+    }
+
+    public static String HashString(string text, string salt = "")
+    {
+        if (String.IsNullOrEmpty(text))
+        {
+            return String.Empty;
+        }
+
+        // Uses SHA256 to create the hash
+        using (var sha = new System.Security.Cryptography.SHA256Managed())
+        {
+            // Convert the string to a byte array first, to be processed
+            byte[] textBytes = System.Text.Encoding.UTF8.GetBytes(text + salt);
+            byte[] hashBytes = sha.ComputeHash(textBytes);
+
+            // Convert back to a string, removing the '-' that BitConverter adds
+            string hash = BitConverter
+                .ToString(hashBytes)
+                .Replace("-", String.Empty);
+
+            return hash;
+        }
+    }
+
+    public static String HashTilePattern(TileBase[] tiles, string salt = "")
+    {
+        return HashString(concatTileNames(tiles), salt);
+    }
+
+    public static string concatTileNames( TileBase[] tiles)
+    {
+        String result = "";
+        foreach(TileBase tile in tiles)
+        {
+            result += tile.name;
+        }
+        return result;
+    }
+    public static IEnumerable<XElement> Elements(this XElement xelement, params string[] names) => xelement.Elements().Where(e => names.Any(n => n == e.Name));
+}
---
+++ Assets/Scripts/wfc/OverlappingTileModel.cs
@@ -0,0 +1,105 @@
+// Copyright (C) 2016 Maxim Gumin, The MIT License (MIT)
+
+using System;
+using System.Linq;
+using System.Xml.Linq;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class OverlappingTileModel  : Model
+{
+    List<TileBase> tiles;
+
+    List<string> tilenames;
+    bool blackBackground;
+    Tilemap tilemap; 
+    List<TileBase[]> patterns;
+
+    public OverlappingTileModel(Tilemap tilemap, bool periodicInput, int width, int height, int N,
+                            bool periodic, bool blackBackground, bool ground, Heuristic heuristic) : 
+                            base(width, height, 1, periodic, heuristic)
+    {
+        this.blackBackground = blackBackground;
+        this.tilemap = tilemap;
+
+        tilemap.orientation = Tilemap.Orientation.XY;
+        BoundsInt bounds = tilemap.cellBounds;
+        TileBase[] allTiles = tilemap.GetTilesBlock(bounds);
+        int SX = bounds.size.x, SY = bounds.size.y;
+
+        int nTiles = tilemap.GetUsedTilesCount();
+        weights = new double[tilemap.GetUsedTilesCount() + 1];
+
+        propagator = new int[4][][];
+
+        patterns = new();
+        Dictionary<String, int> patternIndices = new();
+        List<double> weightList = new();
+        T = tilemap.GetUsedTilesCount() + 1;
+        bool[][][] relations = new bool [T][][];
+
+        static TileBase[] pattern(Func<int, int, TileBase> f, int N)
+        {
+            TileBase[] result = new TileBase[N*N];
+            for (int x=0; x<N; x++)for (int y=0; y<N; y++) result[x + y * N] = f(x, y);
+            return result;
+        }
+
+        int xmax = periodicInput ? SX : SX - N + 1;
+        int ymax = periodicInput ? SY : SY - N + 1;
+        for (int y = 0; y < ymax; y++) for (int x = 0; x < xmax; x++)
+            {
+            TileBase[] p = pattern((dx, dy) => allTiles[(x + dx) % SX + (y + dy) % SY * SX], N);
+            String hash = Helper.HashTilePattern(p);
+            if (patternIndices.TryGetValue(hash, out int index)) weightList[index] = weightList[index] + 1;
+            else
+            {
+                patternIndices.Add(hash, weightList.Count);
+                weightList.Add(1.0);
+                patterns.Add(p);
+            }
+        }
+        weights = weightList.ToArray();
+        this.ground = ground;
+
+        static bool agrees(TileBase[] p1, TileBase[] p2, int dx, int dy, int N)
+        {
+            int xmin = dx < 0 ? 0 : dx, xmax = dx < 0 ? dx + N : N, ymin = dy < 0 ? 0 : dy, ymax = dy < 0 ? dy + N : N;
+            for (int y = ymin; y < ymax; y++) for (int x = xmin; x < xmax; x++) if (p1[x + N * y].name != p2[x - dx + N * (y - dy)].name) return false;
+            return true;
+        };
+
+        propagator = new int[4][][];
+        for (int d = 0; d < 4; d++)
+        {
+            propagator[d] = new int[T][]; // Número de Patrones
+            for (int t = 0; t < T; t++)
+            {
+                List<int> list = new();
+                for (int t2 = 0; t2 < T; t2++) if (agrees(patterns[t], patterns[t2], dx[d], dy[d], N)) list.Add(t2);
+                propagator[d][t] = new int[list.Count];
+                for (int c = 0; c < list.Count; c++) propagator[d][t][c] = list[c];
+            }
+        }
+    }
+
+    public override void Save(string filename)
+    {
+        int[] tilemapData = new int[MX * MY];
+        //TODO
+    }
+
+    public string TextOutput()
+    {
+        var result = new System.Text.StringBuilder();
+        for (int y = 0; y < MY; y++)
+        {
+            for (int x = 0; x < MX; x++) result.Append($"{tilenames[observed[x + y * MX]]}, ");
+            result.Append(Environment.NewLine);
+        }
+        return result.ToString();
+    }
+}
+
